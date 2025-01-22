e1b5188bfccf334853a1a4a839ce7342370184a5
Matias Gumma <matiasgumma@gmail.com>
2025-01-22 10:44:52 -0300

    first commit

---
+++ agent.py
@@ -0,0 +1,17 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+import asyncio
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+async def main():
+    agent = Agent(
+        task="Find a one-way flight from Bali to Oman on 18 January 2025 on Google Flights. Return me the cheapest option.",
+        llm=llm,
+        use_vision=False,
+        save_conversation_path="logs/conversation.json",
+    )
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
